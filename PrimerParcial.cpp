a5cf224e4d45db50eace18a1f5ae007d4cc7ad6b
Okkan Chavez <ochavezg2@miumg.edu.gt>
2022-03-05 15:57:01 -0600

    Tercer inciso, al pedirnos una función en que según los dígitos que ingresó el usuario decir cuantos son pares y sumarlos, tanto el porcentaje he decidido utilizar un ciclo for y dentro de él una toma de decisión que (almacenando los valores) divida dentro de 2 el dígito que ingresó el usuario, si este da cero, entonces contará y se almacenará como par, de lo contrario será impar

--- PrimerParcial.cpp
+++ PrimerParcial.cpp
@@ -3,8 +3,10 @@
 
 using namespace std;
 
+
 void facto(float a);
 void tabla(float a);
+void inciso_tres();
 void datos1();
 void datos2();
 
@@ -18,6 +20,7 @@ setlocale(LC_ALL, "spanish");
 cout<<"Soy un Programa avanzado"<<endl;
 cout<<"1. Factorial de un Número"<<endl;
 cout<<"2. Tablas de Multiplicar"<<endl;
+cout<<"3. Contador de Números"<<endl;
 cout<<"\nIngresa el Número del Inciso de la Operación a Realizar: ";cin>>ola;
 
 
@@ -35,6 +38,12 @@ switch(ola){
 		cout<<"Tablas de Multiplicación"<<endl;	
 		datos1();
 		tabla(num1);
+	break;
+	
+	case 3:
+	system("cls");
+	cout<<"Contador de Números"<<endl;	
+	inciso_tres();
 	break;
 	
 	default:
@@ -70,6 +79,28 @@ void tabla(float a){
 	
 }
 
+void inciso_tres(){
+	int y, i;
+	int sumi =0;
+	int sump =0;
+	int par =0;
+	int impar =0;
+	
+	for(i=1;i<=10;i++){
+		cout<<"("<<i<<"). Ingresa Cualquier Número: ";cin>>y;
+		
+		if(y%2==0){
+			sump=sump+y;
+			par=par+1;
+		}else{
+			sumi=sumi+y;
+			impar=impar+1;
+		}
+	}
+	
+	cout<<"\nLa Suma de los Números Pares es: "<<sump<<endl<<"Total de Números Pares: "<<par<<endl<<"El Promedio de Números Impares es de: "<<(sumi/impar);
+	
+}
 
 
 
